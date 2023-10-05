df9fd5c701c6373fb1c886f956756376eba05c9b
Abdul <metalsquad4u@gmail.com>
2023-10-06 00:18:51 +0300

    creating the s3 bucket for the quickstart visualizer

--- lib/quicksight-visualizer-stack.ts
+++ lib/quicksight-visualizer-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,9 @@ export class QuicksightVisualizerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'QuicksightVisualizerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //create an S3 Buckect to store QuickSight  Dataset
+    const qsBucket = new s3.Bucket(this, 'qsBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+    })
   }
 }
