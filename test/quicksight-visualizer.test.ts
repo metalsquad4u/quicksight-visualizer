17619a2126c0f64a3453ea192476bd9a52942cf4
Abdul <metalsquad4u@gmail.com>
2023-10-05 23:26:44 +0300

    Initial commit

---
+++ test/quicksight-visualizer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as QuicksightVisualizer from '../lib/quicksight-visualizer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/quicksight-visualizer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new QuicksightVisualizer.QuicksightVisualizerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
